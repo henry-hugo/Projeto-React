ce6aec48b4d3409216f7c752fcc31521e1e7d63a
HUGO HENRIQUE OLIVEIRA FRANKLIN LEITE <hugo.hofleite@senacsp.edu.br>
2024-02-07 22:11:52 -0300

    aula1

---
+++ primeira-aula/src/App.js
@@ -0,0 +1,23 @@
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello world</h1>
+//     </div>
+//   );
+// }
+
+import AppBar from "./Components/Aplicar/Aplicar";
+
+const App = () => {
+  return(
+    <>
+    <AppBar/>
+    <h1>hello world</h1>
+    </>
+  )
+ 
+}
+
+export default App;
---
+++ primeira-aula/src/Components/Aplicar/Aplicar.js
@@ -0,0 +1,14 @@
+
+import { Button } from "@mui/material";
+
+function AppBar(){
+    return(
+        <>
+        <button>
+            Click auqi
+        </button>
+        </>
+    );
+}
+
+export default AppBar;
