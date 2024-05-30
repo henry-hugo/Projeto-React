3aa6db22ec51f56996681664db234aad70e4c6a1
Hugo Henrique Oliveira Franklin Leite <111157922+henry-hugo@users.noreply.github.com>
2024-05-29 21:06:33 -0300

    new-proj

--- filme-senac/src/componentes/Maisbuscados/Maisbuscados.js
+++ filme-senac/src/componentes/Maisbuscados/Maisbuscados.js
@@ -4,42 +4,53 @@ import { Container, Titulo, Autor, Ano } from "../styles/filme";
 import { Link } from "../styles/texto";
 
 const Maisbuscados = () => {
-    const[Filmes, setFilmes] = useState([]);
+    const [Filmes, setFilmes] = useState([]);
 
-    useEffect(()=>{
-        const buscarFilmes =async () => {
+    useEffect(() => {
+        const buscarFilmes = async () => {
             try {
                 const resposta = await fetch('http://localhost:8080/minhaRota');
                 const dados = await resposta.json();
-                console.log(dados)
+                console.log(dados);
                 setFilmes(dados);
             } catch (error) {
                 console.error(error);
             }
-        }
+        };
         buscarFilmes();
-    },[])
-    return(
+    }, []);
+
+    // Organize os filmes por categoria
+    const filmesPorCategoria = {};
+    Filmes.forEach((filme) => {
+        if (!filmesPorCategoria[filme.categoria]) {
+            filmesPorCategoria[filme.categoria] = [];
+        }
+        filmesPorCategoria[filme.categoria].push(filme);
+    });
+
+    // Renderize os filmes por categoria e ordem
+    return (
         <div>
-            <h2></h2>
-            {
-                Filmes.map((filmes) =>{
-                    return <>
-                        <Container>
-                            <Link href={filmes.link} target="_blank">
-                            <Titulo>{filmes.titulo}</Titulo>
-                            <Autor>{filmes.categoria}</Autor>
-                            <Ano>{filmes.ano}</Ano>
-                            <ImagemFilme src={filmes.imagem}></ImagemFilme>
+            {Object.keys(filmesPorCategoria).map((categoria) => (
+                <div key={categoria}>
+                    <h2>{categoria}</h2>
+                    {filmesPorCategoria[categoria].map((filme) => (
+                        <Container key={filme.titulo}>
+                            <Link href={filme.link} target="_blank">
+                                <Titulo>{filme.titulo}</Titulo>
+                                <Autor>{filme.categoria}</Autor>
+                                <Ano>{filme.ano}</Ano>
+                                <ImagemFilme src={filme.imagem} />
                             </Link>
                         </Container>
-                    </>
-                })
-            }
+                    ))}
+                </div>
+            ))}
         </div>
-        
-    )
-}
+    );
+};
+
 export default Maisbuscados;
 
 
